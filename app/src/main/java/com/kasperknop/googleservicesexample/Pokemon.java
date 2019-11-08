f7031a5ca06bcd56ddcd87b87e8e643a964d2ce8
KasperKnop <kasperknop@gmail.com>
2019-11-08 16:15:20 +0100

    Initial commit

---
+++ app/src/main/java/com/kasperknop/googleservicesexample/Pokemon.java
@@ -0,0 +1,30 @@
+package com.kasperknop.googleservicesexample;
+
+public class Pokemon {
+
+    private String name;
+    private int number;
+
+    public Pokemon(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public Pokemon() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
---
+++ app/src/main/java/com/kasperknop/googleservicesexample/SignInActivity.java
@@ -0,0 +1,68 @@
+package com.kasperknop.googleservicesexample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SignInActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 42;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        checkIfSignedIn();
+        setContentView(R.layout.signin_activity);
+    }
+
+    private void checkIfSignedIn() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null)
+            goToMainActivity();
+    }
+
+    private void goToMainActivity() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
+    public void signIn(View v) {
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build());
+
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setLogo(R.drawable.logo)
+                .build();
+
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            handleSignInRequest(resultCode);
+        }
+    }
+
+    private void handleSignInRequest(int resultCode) {
+        if (resultCode == RESULT_OK)
+            goToMainActivity();
+        else
+            Toast.makeText(this, "SIGN IN CANCELLED", Toast.LENGTH_SHORT).show();
+    }
+}
